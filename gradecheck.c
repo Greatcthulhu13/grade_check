09f407f7a6d8ef2ce326d2b6726220743364c436
Guffy <144727889+Greatcthulhu13@users.noreply.github.com>
2023-11-11 10:31:37 +0000

    Add files via upload

---
+++ gradecheck.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* D.1 Level 1 number 3 (gradecheck.c) - using if else statements */
+int main()
+{
+	int grade;
+	printf("Enter your grade: ");
+	scanf("%d", &grade);
+	if (grade >=100)
+		printf("FULL MARKS! \n");
+	else if (grade >=70)
+		printf("You got a FIRST! \n");
+	else if (grade >=60)
+		printf("You got a 2:1 \n");
+	else if (grade >=50)
+		printf("You got a 2:2 \n");
+	else if (grade >=40)
+		printf("You turned up! \n");
+	else if (grade < 0)
+		printf("How? \n");
+	else printf("You failed \n");
+	return 0;
+}
+/* Try removing the 'else's */
